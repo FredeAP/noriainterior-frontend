2d63a84fdf2b06d0e088d64eb5fe768d89bd485c
Brian Uceda <monalek22@gmail.com>
2024-11-20 11:30:27 -0500

    d

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   PRODUCTION: true,
   ENV_NAME: 'production',
-  FRONTEND_URL: 'https://api.vercel.app/',
-  BACKEND_BASE_URL: 'https://api.globitokuki.site/',
-  BACKEND_URL: 'https://api.globitokuki.site/api/v1/',
+  FRONTEND_URL: 'https://noriainterior.vercel.app/',
+  BACKEND_BASE_URL: 'https://noriainterior.globitokuki.site/',
+  BACKEND_URL: 'https://noriainterior.globitokuki.site/api/v1/',
 };
